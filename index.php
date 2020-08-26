f9427fbcb566a6e04f144aca93033dc509460b89
jian-lin <jian-lin@mail.chungyo.net>
2020-08-26 10:49:46 +0800

    新增按鈕增加刪除的功能

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    session_start();
+    $_SESSION['itemNum']=0;
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,28 +19,77 @@
 <body>
     <?php
     require_once("header.php");
-    for($i=0;$i<10;$i++)
-    {
+    for ($i = 1; $i < 5; $i++) {
     ?>
-    <div class="wrapper outline" >
-        <div class="wrapper2" id="d1">
-            <img src="img/bubbleTea.jpg" alt="找不到圖片ＱＡＯ">
-            <div class="mid">
-                <p>珍珠奶茶（BubbleTea）$:60</p>
+        <div class="wrapper outline">
+            <div class="wrapper2" id="d1">
+                <img src="img/bubbleTea.jpg" alt="找不到圖片ＱＡＯ">
+                <div class="mid">
+                    <p>珍珠奶茶(BubbleTea)</p><p id="price<?=$i?>" name="price">60</p>
+                    
+                </div>
+            </div>
+            <div class="wrapper" id="d2">
+                <div class="mid">
+                    <button class="btn btn-success" id="btnadd<?=$i?>" name="btnadd"> + </button>
+                </div>
+                <div style="margin-top: 80%;">
+                    <p id="itemcount<?=$i?>" name="itemcount" >0</p>
+                </div>
+                <div class="mid">
+                    <button class="btn btn-danger" id="btnsub<?=$i?>" name="btnsub"> - </button>
+                </div>
+            </div>
+            <div class="wrapper2" id="d3">
+                <div style="margin-top: 40%;">
+                    <p class="total">總額為：</p>
+                </div>
+                <div style="margin-top: 40%;">
+                    <p class="total" id="totalm<?=$i?>" name="totalm">0</p>
+                </div>
+
             </div>
         </div>
-        <div class="wrapper2" id="d2">
-            <button class="btn btn-success"> + </button>
-            <button class="btn btn-danger"> - </button>
-        </div>
-        <div id="d3">
-            <p class="total">總額為：</p>
-        </div>
-    </div>
     <?php
     }
-    require_once("footer.php") 
+    require_once("footer.php")
     ?>
+    <script>
+            $("button[name='btnadd']").click(function(){
+                let count;
+                let nowid=$(this).attr("id").replace("btnadd","");
+                
+                // alert(nowid);
+                item="#itemcount"+nowid;
+                total="#totalm"+nowid;
+                price="#price"+nowid;
+                // alert(item);
+
+                count=$(item).text();
+                count++;
+                $(item).text(count);
+                $(total).text($(price).text()*count);
+                
+            })
+            $("button[name='btnsub']").click(function(){
+                let count;
+                let nowid=$(this).attr("id").replace("btnsub","");
+                
+                // alert(nowid);
+                item="#itemcount"+nowid;
+                total="#totalm"+nowid;
+                price="#price"+nowid;
+                // alert(item);
+
+                count=$(item).text();
+                if(count>0)
+                count--;
+                $(item).text(count);
+                $(total).text($(price).text()*count);
+                
+            })
+
+    </script>
 </body>
 
 </html>
