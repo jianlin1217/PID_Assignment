5f5e8943fbbb9383474391e616b6a153b89ff9cd
jian-lin <jian-lin@mail.chungyo.net>
2020-08-28 17:11:45 +0800

    管理產品列表 修改

--- system/products.php
+++ system/products.php
@@ -12,16 +12,29 @@
     $getProduct=<<<end
     select * from itemList;
     end;
-    
+    $result=mysqli_query($link,$getProduct);
+    while($row=mysqli_fetch_assoc($result))
+    {
+        array_push($proName,$row["itemName"]);
+        array_push($proPrice,$row["itemPrice"]);
+        array_push($proRemain,$row["remainCount"]);
+        array_push($proMeg,$row["ItemMassage"]);
+        array_push($proImgLink,$row["drinkImg"]);
+    }
+    // var_dump($proName);
+    // var_dump($proPrice);
+    // var_dump($proRemain);
+    // var_dump($proMeg);
+    // var_dump($proImgLink);
+
+    //總產品數量
+    $proTotal=count($proName);
+    global $proTotal;
 
     //產品作用按鈕
     if(isset($_POST['btnAdd']))
     {
         echo "新增";
-    }
-    if(isset($_POST['btnMod']))
-    {
-        echo "修改";
     }
 ?>
 
@@ -44,33 +57,74 @@
 </head>
 <body>
     <?php
-        require_once("header.php");
+        // require_once("header.php");
     ?>
     <div class="container" style="margin-top: 100px;">
         <h1>商品總覽</h1>
         <div class="">
-            <form action="" method="post">
                 <button id="btnAdd" name="btnAdd">新增</button>
-                <button id="btnMod" name="btnMod" >修改</button>
-            </form>          
+                <button id="btnMod" name="btnMod">修改</button>        
         </div>
+        <?php
+            for($i=0;$i<$proTotal;$i++)
+            {
+        ?>
         <div class="wrapper bound" style="margin-top: 20px;">
             <img src="../img/bubbleTea.jpg" alt="ＲＲＲＲ">
             <div>
                 <div class="wrapper3 " >
                     <p>
-                        商品名稱<br><br>
-
-                         <?=123456?> 
+                        商品名稱<br><br><br>
+                        <input name="pName" id="pName<?=$i?>" disabled value="<?=$proName[$i]?>">
+                        
                     </p>
-                    <p>商品金額</p>
-                    <p>剩餘數量</p>
+                    <p>
+                        商品金額<br><br><br>
+                        <input name="price" id="price<?=$i?>" disabled value="<?=$proPrice[$i]?>">
+                        
+                    </p>
+                    <p>
+                        剩餘數量<br><br><br>
+                        <input name="remain" id="remain<?=$i?>" disabled value="<?=$proRemain[$i]?> ">
+                        
+                    </p>
                 </div>
             </div>
             <div>
-                <p>商品描述</p>
+                <p>商品描述:<br></p>
+                <textarea name="textF" id="textF<?=$i?>" cols="24" rows="10" style="width: 200px; height:200px" disabled><?=$proMeg[$i]?></textarea>
+                
             </div>
         </div>
+        <?php
+            }
+        ?>
     </div>
+    <button id="btnSend" name="btnSend" style="display: none ;">送出</button>
 </body>
+<script>
+    let flag=true;
+    $("#btnMod").click(function(){
+        
+        //讓管理者可以修改
+        if(flag)
+        {
+            $flag=false;
+            $("input[name='pName']").attr("disabled",false);
+            $("input[name='price']").attr("disabled",false);
+            $("input[name='remain']").attr("disabled",false);
+            $("textarea[name='textF']").attr("disabled",false);
+            $("#btnSend").hide();
+        }   
+        else
+        {
+            $flag=true;
+            $("input[name='pName']").attr("disabled");
+            $("input[name='price']").attr("disabled");
+            $("input[name='remain']").attr("disabled");
+            $("textarea[name='textF']").attr("disabled");
+            $("#btnSend").show();
+        }
+    })
+</script>
 </html>
